5bcee77eb9bf47a62d304002ea138d1c94b4f017
bence-balazs <bestbence@gmail.com>
2024-04-23 15:16:46 +0200

    fix: auto approve

--- auto-setup.sh
+++ auto-setup.sh
@@ -76,8 +76,8 @@ setup_docker() {
     done
 
     # Initialize docker repo.
-    sudo apt-get update
-    sudo apt-get install ca-certificates curl
+    sudo apt-get update -y
+    sudo apt-get install -y ca-certificates curl
     sudo install -m 0755 -d /etc/apt/keyrings
     sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
     sudo chmod a+r /etc/apt/keyrings/docker.asc
